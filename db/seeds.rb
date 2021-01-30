e85b608fd6c65ca711c4769f61a29a865d5125e2
Veronica <veronica.gudova@gmail.com>
2021-01-29 16:22:35 -0800

    added post model and seeded it

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Post.delete_all
+
+# NUM_OF_POSTS=50
+
+    60.times do
+        created_at=Faker::Date.backward(days: 365)
+        Post.create(
+            title: Faker::Lorem.sentence,
+            body: Faker::Lorem.paragraph_by_chars,
+            created_at: created_at,
+            updated_at: created_at
+        )
+    end
+    p=Post.all
+    puts "created #{p.count} posts"
